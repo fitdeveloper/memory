bacb9601a7dcf69d5b7ed82bd59953c58151a2ae
Mahmoud Zine <mahmoud.zine@atos.net>
2019-07-22 14:46:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// // import React from 'react';
+// // import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// // function App() {
+// //   return (
+// //     <p> Hello World this my first React App </p>
+// //   );
+// // }
+
+// // export default App;
+
+// import React, { Component } from 'react'
+// import './App.css'
+// import Card from './Card';
+// import GuessCount from './GuessCount';
+
+// class App extends Component {
+//   handledCardClick(card){
+//     console.log(card,"clicked");
+//   }
+
+//   render() {
+//     const won = new Date().getSeconds() % 2 === 0
+//     return (
+//     <div className="memory"> 
+//     <GuessCount guesses={0} />
+//         <Card card="😀" feedback="hidden" onClick={this.handledCardClick}/>
+//         <Card card="🎉" feedback="justMatched" onClick={this.handledCardClick}/>
+//         <Card card="💖" feedback="justMismatched" onClick={this.handledCardClick}/>
+//         <Card card="🎩" feedback="visible" onClick={this.handledCardClick}/>
+//         <Card card="🐶" feedback="hidden" onClick={this.handledCardClick}/>
+//         <Card card="🐱" feedback="justMatched" onClick={this.handledCardClick}/>
+//         {won && <p>GAGNÉ !</p>}
+//     </div> 
+//     )
+//     // const className = "log entry ${this.isOpen() ? 'open' : 'closed'}"
+//     // return (
+//     //   <li className={className} onClick={this.toggle}>
+//     //     … test 
+//     //   </li>
+//     // )
+//   }
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+// import Card from './Card';
+// import GuessCount from './GuessCount';
+import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame';
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+
+
+class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
+  render() {
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        { this.cards.map((card, index)=> (
+          <Card 
+          card={card}
+          feedback="hidden"
+          key={index}
+          onClick={this.handleCardClick} />
+        )) }
+        <HallOfFame entries={FAKE_HOF} />
+        {/* 
+            const won = new Date().getSeconds() % 2 === 0
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} /> 
+                {won && <p>GAGNÉ !</p>} */}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Card.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+// import './Card.css'
+
+// const HIDDEN_SYMBOL = '❓'
+
+// const Card = ({ card, feedback , onClick }) => (
+//     <div className={'card ${feedback}'}  onClick= {() => {onClick(card)}}>
+//         <span className="symbol">
+//             { feedback === 'hidden' ? HIDDEN_SYMBOL : card }
+//         </span>
+//     </div>
+// )
+
+// export default Card
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({ card, feedback, onClick }) => (
+  <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+    <span className="symbol">
+      {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+    </span>
+  </div>
+)
+
+export default Card
